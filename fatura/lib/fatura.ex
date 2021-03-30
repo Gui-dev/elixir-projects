442f98c2f2426d10f2aa2a157d6fe70f62492f9b
Gui-dev <adrianon10@gmail.com>
2021-03-30 20:58:32 -0300

    refactor: add docs

--- fatura/lib/fatura.ex
+++ fatura/lib/fatura.ex
@@ -7,7 +7,12 @@ defmodule Fatura do
     Ao receber `faturas` retorna um array de faturas
       ## Exemplos
       iex> Fatura.criar_faturas(['Telefone', 'Agua'], [5, 10])
-      ['Telefone', 'Agua']
+      [
+        %Fatura.Conta{fatura: 'Telefone', vencimento: 5},
+        %Fatura.Conta{fatura: 'Agua', vencimento: 5},
+        %Fatura.Conta{fatura: 'Telefone', vencimento: 10},
+        %Fatura.Conta{fatura: 'Agua', vencimento: 10}
+      ]
   """
   def criar_faturas(faturas, vencimentos) do
     for vencimento <- vencimentos, fatura <- faturas do
@@ -16,17 +21,16 @@ defmodule Fatura do
   end
 
   @doc """
-    Ao receber `faturas` e `quantidades` retorna a quantidade de fatura á ser pagas
+    Ao receber `faturas` e `quantidades` retorna um array com as quantidades passada
       ## Exemplos
-      iex> Fatura.faturas_a_pagar(Fatura.criar_faturas(['Telefone', 'Agua'], [5, 10]), 1)
+      iex> faturas = Fatura.criar_faturas(['Telefone', 'Agua'], [5, 10])
+      iex> Fatura.faturas_a_pagar(faturas, 1)
       {
+        [%Fatura.Conta{fatura: 'Telefone', vencimento: 5}],
         [
-          'Fatura: Telefone vence no dia: 5'
-        ],
-        [
-          'Fatura: Agua vence no dia: 5',
-          'Fatura: Telefone vence no dia: 10',
-          'Fatura: Agua vence no dia: 10'
+          %Fatura.Conta{fatura: 'Agua', vencimento: 5},
+          %Fatura.Conta{fatura: 'Telefone', vencimento: 10},
+          %Fatura.Conta{fatura: 'Agua', vencimento: 10}
         ]
       }
   """
@@ -34,11 +38,23 @@ defmodule Fatura do
     Enum.split(faturas, quantidades)
   end
 
-  def save(nome_arquivo, faturas) do
+  @doc """
+    Ao receber o `nome_arquivo` e `faturas` salvar e retornar um :ok
+      ## Exemplo
+      iex> faturas = Fatura.criar_faturas(['Telefone', 'Agua'], [5, 10])
+      iex> Fatura.save(faturas, 'nome_arquivo')
+      :ok
+  """
+  def save(faturas, nome_arquivo) do
     binary = :erlang.term_to_binary(faturas)
     File.write!(nome_arquivo, binary)
   end
 
+  @doc """
+    Ao receber `nome_arquivo` retorna o arquivo
+      ##Exemplos
+      iex> Fatura.load('nome_arquivo')
+  """
   def load(nome_arquivo) do
     { status, file } = File.read(nome_arquivo)
 
@@ -49,17 +65,31 @@ defmodule Fatura do
 
   end
 
-  def pagar_uma_fatura(faturas, vencimento, quantidade) do
+  @doc """
+    Ao receber dados para gerar fatura, deve ordenar e salvar em um arquivo
+      ## Exemplos
+      iex> faturas = Fatura.criar_faturas(['Telefone', 'Agua'], [5, 10])
+      iex> Fatura.pagar_uma_fatura(faturas, [5, 10], 1, 'nome_arquivo')
+      iex> :ok
+  """
+  def pagar_uma_fatura(faturas, vencimento, quantidade, nome_arquivo) do
     criar_faturas(faturas, vencimento)
     |> ordena_fatura
     |> faturas_a_pagar(quantidade)
+    |> save(nome_arquivo)
   end
 
   @doc """
     Ao receber `fatura` retorna um array de faturas ordenado
       ##Exemplos
-      iex> Fatura.ordena_fatura(Fatura.criar_fatura(['Telefone', 'Agua']))
-      ['Agua', 'Telefone']
+      iex> faturas = Fatura.criar_faturas(['Telefone', 'Agua'], [5, 10])
+      iex> Fatura.ordena_fatura(faturas)
+      [
+        %Fatura.Conta{fatura: 'Agua', vencimento: 5},
+        %Fatura.Conta{fatura: 'Agua', vencimento: 10},
+        %Fatura.Conta{fatura: 'Telefone', vencimento: 5},
+        %Fatura.Conta{fatura: 'Telefone', vencimento: 10}
+      ]
   """
   def ordena_fatura(faturas) do
     Enum.sort(faturas)
@@ -68,7 +98,9 @@ defmodule Fatura do
   @doc """
     Ao receber `faturas` e um elemento de `fatura` retorna se ele existe ou não
       ## Exemplos
-      iex> Fatura.fatura_existe?(Fatura.criar_fatura(['Telefone', 'Agua']), 'Luz')
+      iex> faturas = Fatura.criar_faturas(['Telefone', 'Agua'], [5, 10])
+      iex> Fatura.fatura_existe?(faturas, %Fatura.Conta{fatura: 'Telefone', vencimento: 5})
+      iex> true
   """
   def fatura_existe?(faturas, fatura) do
     Enum.member?(faturas, fatura)
