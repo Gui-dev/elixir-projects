cea060bd18eedc820267a193b267054866ceb276
Gui-dev <adrianon10@gmail.com>
2021-03-31 01:28:32 -0300

    first commit

---
+++ icone_identidade/lib/icone_identidade.ex
@@ -0,0 +1,18 @@
+defmodule IconeIdentidade do
+  @moduledoc """
+  Documentation for `IconeIdentidade`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> IconeIdentidade.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ icone_identidade/test/icone_identidade_test.exs
@@ -0,0 +1,8 @@
+defmodule IconeIdentidadeTest do
+  use ExUnit.Case
+  doctest IconeIdentidade
+
+  test "greets the world" do
+    assert IconeIdentidade.hello() == :world
+  end
+end
