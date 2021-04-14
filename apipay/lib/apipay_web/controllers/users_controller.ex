98d1fb8b135f73c37886897aaa0c6e91e92018ae
Gui-dev <adrianon10@gmail.com>
2021-04-14 03:51:18 -0300

    feat: add FallbackController

--- apipay/lib/apipay_web/controllers/users_controller.ex
+++ apipay/lib/apipay_web/controllers/users_controller.ex
@@ -2,22 +2,14 @@ defmodule ApipayWeb.UsersController do
   use ApipayWeb, :controller
   alias Apipay.User
 
+  action_fallback ApipayWeb.FallbackController
+
   def create(conn, params) do
-    params
-    |> Apipay.create_user
-    |> handle_response(conn)
+    with {:ok, %User{} = user} <- Apipay.create_user(params) do
+      conn
+      |> put_status(:created)
+      |> render("create.json", user: user)
+    end
   end
 
-  defp handle_response({:ok, %User{} = user}, conn) do
-    conn
-    |> put_status(:created)
-    |> render("create.json", user: user)
-  end
-
-  defp handle_response({:error, result}, conn) do
-    conn
-    |> put_status(:bad_request)
-    |> put_view(ApipayWeb.ErrorView)
-    |> render("400.json", result: result)
-  end
 end
