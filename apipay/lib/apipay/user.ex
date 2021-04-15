f381d985ffed56a58731cd051729cbbc1d823590
Gui-dev <adrianon10@gmail.com>
2021-04-15 02:58:45 -0300

    feat: add account relationship

--- apipay/lib/apipay/user.ex
+++ apipay/lib/apipay/user.ex
@@ -2,6 +2,7 @@ defmodule Apipay.User do
   use Ecto.Schema
   import Ecto.Changeset
   alias Ecto.Changeset
+  alias Apipay.Account
 
   @primary_key {:id, :binary_id, autogenerate: true}
 
@@ -14,6 +15,7 @@ defmodule Apipay.User do
     field :password, :string, virtual: true
     field :password_hash, :string
     field :nickname, :string
+    has_one :account, Account
     timestamps()
   end
 
