d2cdff06dd47c181d0cc09f4e2e3f8447c4e5324
Gui-dev <adrianon10@gmail.com>
2021-04-16 02:42:38 -0300

    feat: add function delegate deposit

--- apipay/lib/apipay.ex
+++ apipay/lib/apipay.ex
@@ -1,5 +1,7 @@
 defmodule Apipay do
   alias Apipay.Users.Create, as: UserCreate
+  alias Apipay.Accounts.Deposit
 
   defdelegate create_user(params), to: UserCreate, as: :call
+  defdelegate deposit(params), to: Deposit, as: :call
 end
