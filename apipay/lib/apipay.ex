961582a8b311a966787bcebbf5434e09087582d7
Gui-dev <adrianon10@gmail.com>
2021-04-17 03:29:19 -0300

    feat: add facade withdraw

--- apipay/lib/apipay.ex
+++ apipay/lib/apipay.ex
@@ -1,7 +1,8 @@
 defmodule Apipay do
   alias Apipay.Users.Create, as: UserCreate
-  alias Apipay.Accounts.Deposit
+  alias Apipay.Accounts.{Deposit, Withdraw}
 
   defdelegate create_user(params), to: UserCreate, as: :call
   defdelegate deposit(params), to: Deposit, as: :call
+  defdelegate withdraw(params), to: Withdraw, as: :call
 end
