17713a69565e731328dd8cb308caa0d6720bb871
Gui-dev <adrianon10@gmail.com>
2021-04-17 03:12:39 -0300

    refactor: remove IO.inspect

--- apipay/lib/apipay_web/controllers/fallback_controller.ex
+++ apipay/lib/apipay_web/controllers/fallback_controller.ex
@@ -2,7 +2,6 @@ defmodule ApipayWeb.FallbackController do
   use ApipayWeb, :controller
 
   def call(conn, {:error, result}) do
-    IO.puts("Fui Chamado")
     conn
     |> put_status(:bad_request)
     |> put_view(ApipayWeb.ErrorView)
