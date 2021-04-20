af95f3be9bd30d321bbc1da4bbd7dfce729534a1
Gui-dev <adrianon10@gmail.com>
2021-04-20 03:52:04 -0300

    feat: add a new delegate transaction

--- apipay/lib/apipay.ex
+++ apipay/lib/apipay.ex
@@ -1,8 +1,9 @@
 defmodule Apipay do
   alias Apipay.Users.Create, as: UserCreate
-  alias Apipay.Accounts.{Deposit, Withdraw}
+  alias Apipay.Accounts.{Deposit, Transaction, Withdraw}
 
   defdelegate create_user(params), to: UserCreate, as: :call
   defdelegate deposit(params), to: Deposit, as: :call
   defdelegate withdraw(params), to: Withdraw, as: :call
+  defdelegate transaction(params), to: Transaction, as: :call
 end
