b889fd5e0670adef197536d045904256ec2367d6
Gui-dev <adrianon10@gmail.com>
2021-04-22 03:38:35 -0300

    test: add when all params are valid, make the deposit

---
+++ apipay/test/apipay_web/controllers/accounts_controller_test.exs
@@ -0,0 +1,37 @@
+defmodule ApipayWeb.AccountsControllerTest do
+  use ApipayWeb.ConnCase
+
+  alias Apipay.{Account, User}
+
+  describe "deposit/2" do
+    setup %{conn: conn} do
+      params = %{
+        name: "Gui Silva",
+        email: "gui@email.com",
+        password: "1234567",
+        nickname: "gui",
+        age: 36
+      }
+
+      {:ok, %User{account: %Account{id: account_id}}} = Apipay.create_user(params)
+
+      conn = put_req_header(conn, "authorization", "Basic YWRtaW46MTIzNDU2")
+
+      {:ok, conn: conn, account_id: account_id}
+    end
+
+    test "when all params are valid, make the deposit", %{conn: conn, account_id: account_id} do
+      params = %{"value" => "50.00"}
+
+      response =
+        conn
+        |> post(Routes.accounts_path(conn, :deposit, account_id, params))
+        |> json_response(:ok)
+
+      assert %{
+        "account" => %{"balance" => "50.00", "id" => _id},
+        "message" => "Balance changes successfully"
+      } = response
+    end
+  end
+end
